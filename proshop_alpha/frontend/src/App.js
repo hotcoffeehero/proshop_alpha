7d4630da3dc03de180d00407683b9265014d0b5b
hotcoffeehero <hotcoffeehero@gmail.com>
2020-11-26 11:15:31 +0900

    7 Rating Component

--- proshop_alpha/frontend/src/App.js
+++ proshop_alpha/frontend/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
-
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-s'>
+      <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
