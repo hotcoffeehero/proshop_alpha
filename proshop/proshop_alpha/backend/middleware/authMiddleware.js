e6dd0ad7c985ca46b3c41890aaa420229292fecd
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-02 16:51:27 +0900

    66 Admin Middleware & Get Users Endpoint

--- proshop/proshop_alpha/backend/middleware/authMiddleware.js
+++ proshop/proshop_alpha/backend/middleware/authMiddleware.js
@@ -30,4 +30,14 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 })
 
-export { protect }
+const admin = (req, res, next ) => {
+  if(req.user && req.user.isAdmin){
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not Authorized as an Admin')
+    //Remember that your error messages need messages
+  }
+}
+
+export { protect, admin }
