201dd6c5387da9a23a24957956d40c0929099b7a
hotcoffeehero <hotcoffeehero@gmail.com>
2020-12-04 15:48:46 +0900

    69 Admin User Delete

--- proshop/proshop_alpha/backend/controllers/userController.js
+++ proshop/proshop_alpha/backend/controllers/userController.js
@@ -110,7 +110,27 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({})
   res.json(users)
-
-  
 })
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+
+// @desc    Delete users
+// @route   Delete /api/users/:id
+// @access  Private/admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+    if (user) {
+      await user.remove()
+      res.json({message: 'User Removed'})
+    }else{
+      res.status(404)
+      throw new Error('User Not Found')
+    }
+})
+
+export { 
+  authUser, 
+  registerUser, 
+  getUserProfile, 
+  updateUserProfile, 
+  getUsers,
+  deleteUser 
+}
