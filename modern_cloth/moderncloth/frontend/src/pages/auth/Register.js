8a290478acec466a43b429ced0ec9ec921ca9532
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-07 19:14:30 +0900

    Register Form

--- modern_cloth/moderncloth/frontend/src/pages/auth/Register.js
+++ modern_cloth/moderncloth/frontend/src/pages/auth/Register.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react"
 
 const Register = () => {
-    return (
-        <div>
-            Register. Gafe. 
+  const [email, setEmail] = useState("")
+
+  const submitHandler = () => {}
+
+  const registerForm = () => (
+    <form onSubmit={submitHandler}>
+      <input
+        type="email"
+        className="form-control"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        autofocus
+      />
+
+      <button type='submit' className='btn btn-raised py-3' >
+        REGISTER
+      </button>
+    </form>
+  )
+
+  return (
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Register</h4>
+          {registerForm()}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Register
