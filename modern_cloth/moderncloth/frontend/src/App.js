e61d2ed7958bbc852d4527031bdaa98741313e21
hotcoffeehero <hotcoffeehero@gmail.com>
2021-01-07 13:26:06 +0900

    Reboot: Routes & Pages

---
+++ modern_cloth/moderncloth/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
+  )
+}
+
+export default App
---
+++ modern_cloth/moderncloth/frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Home. Kerdouche. 
+        </div>
+    )
+}
+
+export default Home
